9ae51f085cfde3cd65bb6c9aa4fe52aecf3d9705
BonyChops <office.kentarou@gmail.com>
2021-02-14 12:42:45 +0900

    Edit: v1.2

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const tsunami = {
     Warning: "津波予報[種類不明]"
 }
 
-let bufDate = moment();
+let bufDate = moment(0);
 
 const discordPost = (json) => {
     console.log(JSON.stringify(json, null, 2));
@@ -47,13 +47,13 @@ const discordPost = (json) => {
 }
 
 const job = () => {
-    request({ url: `https://api.p2pquake.net/v1/human-readable?limit=3`, headers: { 'User-Agent': 'nnct3j-bot' } }, (error, response, body) => {
+    request({ url: `https://api.p2pquake.net/v1/human-readable?limit=100`, headers: { 'User-Agent': 'nnct3j-bot' } }, (error, response, body) => {
         let data = JSON.parse(response.body);
         data.forEach(data => data.time = moment(data.time.split("/").join("-")));
         const dataFromKisho = data.filter(obj => obj.code === 551).filter(obj => obj.earthquake.maxScale >= 30);
-        const dataFast = data.filter(obj => obj.code === 5610)
+        const dataFast = data.filter(obj => obj.code === 5610);
         if (dataFromKisho.some(obj => obj.time > bufDate)) {
-            const data = dataFromKisho.find(obj => obj.time > bufDate);
+            const data = dataFromKisho.find(obj => obj.time > bufDate && obj.points.some(point => point.addr.indexOf(config.importantArea) !== -1));
             //const data = dataFromKisho[1];
             const isDanger = data.points.some(point => point.addr.indexOf(config.importantArea) !== -1);
             if (discordPost({
@@ -64,9 +64,9 @@ const job = () => {
                     {
                         "title": `気象庁より地震速報がありました`,
                         "description": `${data.earthquake.time}ごろ地震がありました．`
-                            + (isDanger ? `**${config.importantArea}での震度は${(shindo[data.points.find(point => point.addr.indexOf(config.importantArea) !== -1).scale])}です．落ち着いて，身を守る行動を取ってください．みんなで生き残ろう．**`
-                                : `${config.importantArea}への影響はありませんでした．`),
-                        "timestamp": moment().utc(),
+                            + (isDanger ? `**${config.importantArea}での震度は${(shindo[data.points.find(point => point.addr.indexOf(config.importantArea) !== -1).scale])}です．落ち着いて，身を守る行動を取ってください．**`
+                                : `${config.importantArea}への影響はありません．`),
+                        "timestamp": data.time.utc(),
                         "color": (isDanger) ? 0xFF0000 : 0xFFFF00,
                         "thumbnail": {
                             "url": (isDanger) ? "https://cdn1.iconfinder.com/data/icons/color-bold-style/21/08-512.png" : "https://cdn.iconscout.com/icon/free/png-256/warning-notice-sign-symbol-38020.png"
@@ -94,7 +94,11 @@ const job = () => {
                         }]).concat(data.points.reduce((a, c) => {
                             let t = a.find(v => v.scale == c.scale);
                             if (t !== undefined) {
-                                t.addr.push(c.addr);
+                                if(c.addr.indexOf(config.importantArea) !== -1){
+                                    t.addr.unshift(c.addr);
+                                }else{
+                                    t.addr.push(c.addr);
+                                }
                             } else {
                                 a.push({
                                     scale: c.scale,
@@ -105,7 +109,65 @@ const job = () => {
                         }, []).map(v => ({
                             name: `震度 ${shindo[v.scale]}`,
                             value: v.addr.join(", ").substr(0, 50)
-                        })))
+                        }))).concat([
+                            {
+                                name: "地震情報をTwitterで確認する",
+                                value: "[リンク](https://twitter.com/search?q=%E5%9C%B0%E9%9C%87+%E9%80%9F%E5%A0%B1)"
+                            },
+                            {
+                                name: "Twitter: 特務機関NERV",
+                                value: "[リンク](https://twitter.com/UN_NERV)"
+                            },{
+                                name: "気象庁 | 地震情報",
+                                value: "https://www.jma.go.jp/jp/quake/"
+                            }
+                        ])
+                    }
+                ]
+            })) {
+                bufDate = moment(data.time);
+            }
+        } else if (dataFast.some(obj => obj.time > bufDate && (Object.keys(obj.areas).some(name => name.indexOf(config.importantArea) !== -1)))) {
+            console.log("it has");
+            const data = dataFast.find(obj => obj.time > bufDate && (Object.keys(obj.areas).some(name => name.indexOf(config.importantArea) !== -1)));
+            if (discordPost({
+                "username": "緊急地震速報",
+                "avatar_url": "https://cdn1.iconfinder.com/data/icons/color-bold-style/21/08-512.png",
+                "content": `@everyone **${config.importantArea}で地震発生．隠れて！**`,
+                "embeds": [
+                    {
+                        "title": `地震を観測しました`,
+                        "description": `**${config.importantArea}**を含むエリアで地震を観測しました．**隠れて！**`,
+                        "timestamp": data.time.utc(),
+                        "color": 0xFF0000,
+                        "thumbnail": {
+                            "url": "https://cdn1.iconfinder.com/data/icons/color-bold-style/21/08-512.png",
+                        },
+                        fields: ([{
+                            name: `${config.importantArea}での地震感知情報の総件数`,
+                            value: data.prefs[Object.keys(data.prefs).find(name => name.indexOf(config.importantPref) !== -1)] + " 件",
+                            inline: false
+                        }]).concat(Object.keys(data.areas).filter(name => name.indexOf(config.importantArea) !== -1).map(name => ({
+                            name,
+                            value: data.areas[name] + " 件",
+                            inline: true
+                        }))).concat(Object.keys(data.areas).filter(name => name.indexOf(config.importantArea) === -1).map(name => ({
+                            name,
+                            value: data.areas[name] + " 件",
+                            inline: true
+                        }))).concat([
+                            {
+                                name: "地震情報をTwitterで確認する",
+                                value: "[リンク](https://twitter.com/search?q=%E5%9C%B0%E9%9C%87+%E9%80%9F%E5%A0%B1)"
+                            },
+                            {
+                                name: "Twitter: 特務機関NERV",
+                                value: "[リンク](https://twitter.com/UN_NERV)"
+                            },{
+                                name: "気象庁 | 地震情報",
+                                value: "https://www.jma.go.jp/jp/quake/"
+                            }
+                        ])
                     }
                 ]
             })) {
